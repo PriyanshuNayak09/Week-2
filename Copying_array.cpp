7018a1ec0360ed811bf8f212e0ccd4b4dcca7d52
PriyanshuNayak09 <163439750+PriyanshuNayak09@users.noreply.github.com>
2024-05-31 23:08:28 +0530

    _

--- Copying_array.cpp
+++ Copying_array.cpp
@@ -3,27 +3,27 @@ using namespace std;
 int main()
 {
     int n;
-    cout << "Enter the size of the array: ";
-    cin >> n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
 
     int arr[n];
     int carr[n];
 
     cout<<"Enter elements of the original array: ";
-    for (int i=0; i<n; i++)
+    for(int i=0; i<n; i++)
     {
         cin>>arr[i];
     }
 
-    for (int i = 0; i < n; i++)
+    for(int i=0; i<n; i++)
     {
-        carr[i] = arr[i];
+        carr[i]=arr[i];
     }
 
-    cout << "Elements of the copied array : ";
-    for (int i = 0; i < n; i++)
+    cout<<"Elements of the copied array: ";
+    for (int i=0; i<n; i++)
     {
-        cout << carr[i] << " "<< endl;
+        cout<<carr[i]<<" "<<endl;
     }
     return 0;
 }
